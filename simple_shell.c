004aa4cbcc5a4f481477bf0e5d6d08d1de9715a2
aurelien <aurelienmest@outlook.fr>
2024-12-19 17:22:04 +0100

    chal8

--- simple_shell.c
+++ simple_shell.c
@@ -7,33 +7,33 @@
 
 void execute_command(char *command)
 {
-    pid_t pid;
-    char *args[100];
-    char *token = strtok(command, " ");
-    int i = 0;
+	pid_t pid;
+	char *args[100];
+	char *token = strtok(command, " ");
+	int i = 0;
 
-    while (token != NULL && i < 99)
-    {
-        args[i++] = token;
-        token = strtok(NULL, " ");
-    }
-    args[i] = NULL;
+	while (token != NULL && i < 99)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
 
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        return;
-    } else if (pid == 0) {
+	pid = fork();
+	if (pid == -1) {
+		perror("fork");
+		return;
+	} else if (pid == 0) {
 
-        if (execvp(args[0], args) == -1)
-        {
-            perror("exec");
-            exit(EXIT_FAILURE);
-        }
-    }
+		if (execvp(args[0], args) == -1)
+		{
+			perror("exec");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 int main() {
-    char command[] = "ls -l";
-    execute_command(command);
-    return 0;
+	char command[] = "ls -l";
+	execute_command(command);
+	return 0;
 }
